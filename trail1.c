8ebdf57f9647a04c63e99676078af4b1395ef2fc
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-03-08 11:49:05 +0530

    Update trail1.c

--- trail1.c
+++ trail1.c
@@ -2,16 +2,37 @@
 #include <assert.h>
 
 int readings[] = {3,3,5,4,10,11,12};
+_Bool checkLowerLimit(int lowerlimit,int readings)
+{
+    if (readings>=lowerlimit)
+    return 1;
+    else 
+    return 0;
+
+}
+_Bool checkUpperLimit(int upperlimit,int readings)
+{
+    if(readings<=upperlimit)
+    return 1;
+    else 
+    return 0;
+}
+int checkWithinRange(_Bool Lowerresult,_Bool Upperresult)
+{
+    if (Lowerresult && Upperresult)
+    return(1);
+    else
+    return(0);
+}
 
 int countInContinuousRange(int lowerlimit, int upperlimit)
 {
     int Count=0;
     for(int readingIterator = 0; readingIterator<= 8 ;readingIterator++)
     {
-   if ((readings[readingIterator]>=lowerlimit ))// && (readings[readingIterator]<=upperlimit ))
-    {
-        Count++;
-    }
+       _Bool Lowerresult = checkLowerLimit(lowerlimit,readings[readingIterator]);
+    _Bool   Upperresult =  checkUpperLimit(upperlimit,readings[readingIterator]);
+     Count +=  checkWithinRange(Lowerresult,Upperresult);
     }
    
      return Count;
@@ -23,17 +44,11 @@ int main()
 	
 int low = 3;
 int high = 5;
-//char buff[5];
-  
+char buff[5];
+     assert((countInContinuousRange(3,5)==4)==1);
     int a = countInContinuousRange(low,high);
-   //   printf("%d\n",a);
-     //  assert((countInContinuousRange(3,5)==4)==1);
        sprintf(buff,"%d-%d,%d",low,high,a);
        printf("%s",buff);
-  /*    FILE *fp;
-  
-    fp = fopen("Record.csv","a");
-    fwrite(&buff, sizeof(buff), 1, fp);
-    fclose(fp);*/
+
     
 }
