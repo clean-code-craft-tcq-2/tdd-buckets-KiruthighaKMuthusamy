5da103fcddd1ef009f6e29840071972b03dd0905
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-03-09 12:45:21 +0530

    Update TestCurrentRangeAndReadings.cpp

--- TestCurrentRangeAndReadings.cpp
+++ TestCurrentRangeAndReadings.cpp
@@ -3,8 +3,16 @@
 #include "test-framework/catch.hpp"
 
 int checkRangeAndReadings(int *readings,int length);
+// Attempt 1. 
+/* TEST_CASE("Checks the charging current range and its occurences with small samples") {                                      //FAILED
+	int chargingCurrentSamples[] = {4,5};
+	int expectedNumOfRange = 1;
+	int actualNumOfRange = checkRangeAndReadings(chargingCurrentSamples,2);
+	REQUIRE(expectedNumOfRange == actualNumOfRange);
+} */ //The above test failed because the actualNumOfRange is not properly incremented
 
-TEST_CASE("Checks the charging current range and its occurences with small samples") {
+// Attempt 2. 
+TEST_CASE("Checks the charging current range and its occurences with small samples") {                                      //SAME TEST CASE 
 	int chargingCurrentSamples[] = {4,5};
 	int expectedNumOfRange = 1;
 	int actualNumOfRange = checkRangeAndReadings(chargingCurrentSamples,2);
