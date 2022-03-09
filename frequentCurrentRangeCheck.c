f5649e31104c90ae0098c3631834a6907af888de
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-03-09 10:13:51 +0530

    Add files via upload

---
+++ frequentCurrentRangeCheck.c
@@ -0,0 +1,43 @@
+#include"frequentCurrentRange.h"
+
+void printOnConsole(int readingslowRange,int readingshighRange,int count)
+{   char buff[17];
+   sprintf(buff,"%d-%d,%d\n",readingslowRange,readingshighRange,count);
+       printf("%s",buff);
+}
+
+void findContinuousSamples(int *readings,size_t length)
+{
+    int lowRange = 0;
+    int highRange = 0;
+    int readingsCount = 0;
+	int numOfContinuosSamples = 0;
+    for (int i=1;i<length;i++)
+    { 
+		if( readings[i] - readings[i-1] > 1 )
+        {   
+		highRange =i-1;         		
+		 readingsCount = countInContinuousRange(lowRange,highRange);
+		 printOnConsole(readings[lowRange],readings[highRange],readingsCount);
+		 highRange = i;
+         lowRange = i;
+		 numOfContinuosSamples++;
+          
+        }
+        else if(i==length-1)
+        {     
+			  highRange = i;
+			  readingsCount = countInContinuousRange(lowRange,highRange);
+		      printOnConsole(readings[lowRange],readings[highRange],readingsCount);
+			  numOfContinuosSamples++;
+        }
+           
+    }
+}
+
+checkRangeAndReadings(int *readings,int length)
+{ 
+    arraySort(readings, length);
+    findContinuousSamples(readings, length);
+}
+
---
+++ sortCurrentRange.c
@@ -0,0 +1,25 @@
+#include"frequentCurrentRange.h"
+
+void swapIfInWrongOrder (int *firstElement,int *secondElement)
+{	 
+    if(*firstElement > *secondElement)
+            {
+    int temp = 0;
+	 temp = *firstElement;
+     *firstElement = *secondElement;
+     *secondElement = temp;
+            }
+}
+
+
+void arraySort(int *readings,size_t length)
+{ 
+    for (int majorIndex = 0; majorIndex<length; majorIndex++)
+    {
+        for (int minorIndex = majorIndex+1; minorIndex<=length-1; minorIndex++)
+        {
+			swapIfInWrongOrder(&readings[majorIndex], &readings[minorIndex]);
+			
+        }
+    }
+}
