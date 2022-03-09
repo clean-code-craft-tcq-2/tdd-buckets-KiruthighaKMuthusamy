7f173db42b99a488971ea3c51c508367134b6a5b
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-03-09 10:20:16 +0530

    Update frequentCurrentRangeCheck.c

--- frequentCurrentRangeCheck.c
+++ frequentCurrentRangeCheck.c
@@ -1,43 +1,50 @@
 #include"frequentCurrentRange.h"
 
 void printOnConsole(int readingslowRange,int readingshighRange,int count)
-{   char buff[17];
-   sprintf(buff,"%d-%d,%d\n",readingslowRange,readingshighRange,count);
+{      char buff[17];
+       sprintf(buff,"%d-%d,%d\n",readingslowRange,readingshighRange,count);
        printf("%s",buff);
 }
+int countInContinuousRange(int lowRange,int highRange)
 
-void findContinuousSamples(int *readings,size_t length)
+{
+    return highRange-lowRange+1;
+}
+int findContinuousSamples(int *readings,size_t length)
 {
     int lowRange = 0;
     int highRange = 0;
     int readingsCount = 0;
-	int numOfContinuosSamples = 0;
+    int numOfContinuosSamples = 0;
+	
     for (int i=1;i<length;i++)
     { 
 		if( readings[i] - readings[i-1] > 1 )
         {   
-		highRange =i-1;         		
+		 highRange =i-1;         		
 		 readingsCount = countInContinuousRange(lowRange,highRange);
 		 printOnConsole(readings[lowRange],readings[highRange],readingsCount);
 		 highRange = i;
-         lowRange = i;
+                  lowRange = i;
 		 numOfContinuosSamples++;
           
         }
         else if(i==length-1)
         {     
-			  highRange = i;
-			  readingsCount = countInContinuousRange(lowRange,highRange);
-		      printOnConsole(readings[lowRange],readings[highRange],readingsCount);
-			  numOfContinuosSamples++;
+		   highRange = i;
+	           readingsCount = countInContinuousRange(lowRange,highRange);
+		   printOnConsole(readings[lowRange],readings[highRange],readingsCount);
+		   numOfContinuosSamples++;
         }
            
     }
+	return numOfContinuosSamples;
 }
 
-checkRangeAndReadings(int *readings,int length)
-{ 
+int checkRangeAndReadings(int *readings,int length)
+{   int numOfContinuosSamples =0;
     arraySort(readings, length);
-    findContinuousSamples(readings, length);
+    numOfContinuosSamples=  findContinuousSamples(readings, length);
+    return numOfContinuosSamples;
 }
 
