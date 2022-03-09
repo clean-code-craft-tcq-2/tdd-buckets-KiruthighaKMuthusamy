c759e58a01e1b11396536b18c85914daee277d1b
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-03-09 11:29:57 +0530

    Update TestCurrentRangeAndReadings.cpp

--- TestCurrentRangeAndReadings.cpp
+++ TestCurrentRangeAndReadings.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "catch.hpp"
+#include "test-framework/catch.hpp"
 #include"frequentCurrentRange.h"
 
 TEST_CASE("Checks the charging current range and its occurences with small samples") {
