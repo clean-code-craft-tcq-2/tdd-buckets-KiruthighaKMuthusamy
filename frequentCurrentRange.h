72201f0f5a50c9bd704fe0f742f3c6336096418e
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-03-09 10:18:40 +0530

    Update frequentCurrentRange.h

--- frequentCurrentRange.h
+++ frequentCurrentRange.h
@@ -2,4 +2,10 @@
 #include <assert.h>
 #include <stdbool.h>
 
+int checkRangeAndReadings(int *readings,int length);
 void arraySort(int *readings,size_t length);
+void swapIfInWrongOrder (int *firstElement,int *secondElement);
+int findContinuousSamples(int *readings,size_t length);
+int countInContinuousRange(int lowRange,int highRange);
+void printOnConsole(int readingslowRange,int readingshighRange,int count);
+
