bd558b661892b4d655a28b13e665c75174c0a1b4
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-03-09 12:41:43 +0530

    Update TestCurrentRangeAndReadings.cpp

--- TestCurrentRangeAndReadings.cpp
+++ TestCurrentRangeAndReadings.cpp
@@ -2,6 +2,7 @@
 
 #include "test-framework/catch.hpp"
 
+int checkRangeAndReadings(int *readings,int length);
 
 TEST_CASE("Checks the charging current range and its occurences with small samples") {
 	int chargingCurrentSamples[] = {4,5};
