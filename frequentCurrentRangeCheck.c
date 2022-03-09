ee9edec8083823ebe8e80d5a38dd03e2cd866141
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-03-09 11:55:13 +0530

    Add files via upload

---
+++ frequentCurrentRangeCheck.c
@@ -0,0 +1,34 @@
+#include"frequentCurrentRange.h"
+
+
+int countInContinuousRange(int lowRange,int highRange)
+
+{
+    return highRange-lowRange;
+}
+int findContinuousSamples(int *readings,int length)
+{
+    int lowRange = 0;
+    int highRange = 0;
+    int readingsCount = 0;
+    int numOfContinuosSamples = 0;
+	
+    for (int i=1;i<length;i++)
+    { 
+	 if(i==length-1)
+        {     
+		   highRange = i;
+	       readingsCount = countInContinuousRange(lowRange,highRange);
+		   	   
+        }
+           
+    }
+	return numOfContinuosSamples;
+}
+
+int checkRangeAndReadings(int *readings,int length)
+{   int numOfContinuosSamples =0;
+    numOfContinuosSamples=  findContinuousSamples(readings, length);
+    return numOfContinuosSamples;
+}
+
