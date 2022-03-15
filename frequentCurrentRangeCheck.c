ca70496677cb058277c6062a4aeb489695535b94
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-03-15 18:46:20 +0530

    Update frequentCurrentRangeCheck.c

--- frequentCurrentRangeCheck.c
+++ frequentCurrentRangeCheck.c
@@ -5,7 +5,6 @@ int countInContinuousRange (int lowRange, int highRange)
   return highRange - lowRange + 1;
 }
 
-
 void storeCalculatedRageReadings(int upperReadings,int lowerReadings,int readingsCount,int numOfContinuosSamples, rangeAndReadings *output)
 {
    	  output[numOfContinuosSamples].upperLimit = upperReadings;
@@ -13,11 +12,9 @@ void storeCalculatedRageReadings(int upperReadings,int lowerReadings,int reading
 	  output[numOfContinuosSamples].readingsCount = readingsCount;
 	  
 }
-
-
-rangeAndReadings* findContinuousSamples (int *readings, int length)
+rangeAndReadings* findContinuousSamples (int *readings, int length, void(*fpPrintOnConsole)(rangeAndReadings*,int))
 {
-  rangeAndReadings *output =
+  rangeAndReadings *rangeAndNoOfReadings =
     (rangeAndReadings *) calloc (10, sizeof (rangeAndReadings));
 
   int lowRange = 0, highRange = 0, readingsCount = 0, numOfContinuosSamples =
@@ -25,35 +22,47 @@ rangeAndReadings* findContinuousSamples (int *readings, int length)
 
   for (int i = 1; i < length; i++)
     {
-      if (readings[i] - readings[i - 1] > 1)
+     if (readings[i] - readings[i - 1] > 1)
 	{
 	  highRange = i - 1;
 	  readingsCount = countInContinuousRange (lowRange, highRange);
-	  storeCalculatedRageReadings(readings[highRange],readings[lowRange],readingsCount,numOfContinuosSamples,output);
-      printOnConsole (output,numOfContinuosSamples); 
+	    storeCalculatedRageReadings(readings[highRange],readings[lowRange],readingsCount,numOfContinuosSamples,output);
+      fpPrintOnConsole (rangeAndNoOfReadings,numOfContinuosSamples); 
 	  numOfContinuosSamples++;
 	  highRange = i;
 	  lowRange = i;
+
+
 	}
       else if (i == length - 1)
 	{
 	  highRange = i;
 	  readingsCount = countInContinuousRange (lowRange, highRange);
-          storeCalculatedRageReadings(readings[highRange],readings[lowRange],readingsCount,numOfContinuosSamples,output);
-	  printOnConsole (output,numOfContinuosSamples);
+	    storeCalculatedRageReadings(readings[highRange],readings[lowRange],readingsCount,numOfContinuosSamples,output);
+	  fpPrintOnConsole (rangeAndNoOfReadings,numOfContinuosSamples);
 	  numOfContinuosSamples++;
 	}
-
-
     }
-  return output;
+  return rangeAndNoOfReadings;
 }
 
-rangeAndReadings* findRangeAndReadings (int *readings, int length)
+rangeAndReadings* checkRangeAndReadings (int *readings, int length,bool(*fpValidateArray)(int* ,int ),void(*fpPrintOnConsole)(rangeAndReadings*,int) , void(*fpPrintErrorMessage)(void))
 {
-    rangeAndReadings* numOfContinuosSamples = 0;
-  numOfContinuosSamples = findContinuousSamples (readings, length);
+    rangeAndReadings* numOfContinuosSamples ;
+    bool validation = fpValidateArray(readings,length);
+  if(validation ==true)  
+  {
+   int* duplicateArray = copyInputArray(readings,  length);
+  sortInputArray(duplicateArray, length);
+  numOfContinuosSamples = findContinuousSamples (duplicateArray, length);
   return numOfContinuosSamples;
-  
+  }
+  else
+  {
+      fpPrintErrorMessage();
+	  numOfContinuosSamples[0].upperLimit = "/0";
+      numOfContinuosSamples[0].lowerLimit = "/0";
+      numOfContinuosSamples[0].readingsCount = "/0";
+  }
    
 }
