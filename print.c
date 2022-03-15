547edb76b65fa2693c61ef24ceecd8865abb0ef5
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-03-15 18:46:46 +0530

    Update print.c

--- print.c
+++ print.c
@@ -1,9 +1,15 @@
 #include"frequentCurrentRange.h"
 
-void printOnConsole (rangeAndReadings* output,int numOfContinuosSamples)
+void printOnConsole (rangeAndReadings* rangeAndNoOfReadings,int numOfContinuosSamples)
 {
   char buff[17];
-  sprintf (buff, "%d-%d,%d\n", output[numOfContinuosSamples].lowerLimit, output[numOfContinuosSamples].upperLimit, output[numOfContinuosSamples].readingsCount);
+  sprintf (buff, "%d-%d,%d\n", rangeAndNoOfReadings[numOfContinuosSamples].lowerLimit, rangeAndNoOfReadings[numOfContinuosSamples].upperLimit, rangeAndNoOfReadings[numOfContinuosSamples].readingsCount);
  printf ("%s", buff);
 }
 
+void printErrorMessage ()
+{
+   fprintf(stderr, "Input is Invalid");
+   exit(EXIT_FAILURE);
+  
+}
