7c5206ef5165aa8c1997ed25634a8ea062522cb0
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-03-15 12:46:11 +0530

    Update frequentCurrentRangeCheck.c

--- frequentCurrentRangeCheck.c
+++ frequentCurrentRangeCheck.c
@@ -1,41 +1,54 @@
-struct RangeAndReadings
-{
-     int LowerSample;
-     int UpperSample;
-     int RangeCount
-};
+#include"frequentCurrentRange.h"
 
-int countInContinuousRange(int lowRange,int highRange)
-
+int countInContinuousRange (int lowRange, int highRange)
 {
-    return highRange-lowRange;
+  return highRange - lowRange + 1;
 }
-RangeAndReadings* findContinuousSamples(int *readings,int length)
-{   struct RangeAndReadings currentRangeReadings[15];
-    int lowRange = 0;
-    int highRange = 0;
-    int readingsCount = 0;
-    int numOfContinuosSamples = 0;
-	
-    for (int i=1;i<length;i++)
-    { 
-	 if(i==length-1)
-        {     
-		   highRange = i;
-	         readingsCount = countInContinuousRange(lowRange,highRange);
-		 numOfContinuosSamples++;
-		 currentRangeReadings[numOfContinuosSamples].LowerSample = readingsCount[lowRange];
-		 currentRangeReadings[numOfContinuosSamples].UpperSample = readingsCount[highRange];
-		 currentRangeReadings[numOfContinuosSamples].RangeCount = readingsCount;
-        }
-           
+
+rangeAndReadings* findContinuousSamples (int *readings, int length)
+{
+  rangeAndReadings *output =
+    (rangeAndReadings *) calloc (10, sizeof (rangeAndReadings));
+
+  int lowRange = 0, highRange = 0, readingsCount = 0, numOfContinuosSamples =
+    0;
+
+  for (int i = 1; i < length; i++)
+    {
+      if (readings[i] - readings[i - 1] > 1)
+	{
+	  highRange = i - 1;
+	  readingsCount = countInContinuousRange (lowRange, highRange);
+	  output[numOfContinuosSamples].upperLimit = highRange;
+	  output[numOfContinuosSamples].lowerLimit = lowRange;
+	  output[numOfContinuosSamples].readingsCount = readingsCount;
+      printOnConsole (output,numOfContinuosSamples); 
+	  numOfContinuosSamples++;
+	  highRange = i;
+	  lowRange = i;
+
+
+	}
+      else if (i == length - 1)
+	{
+	  highRange = i;
+	  readingsCount = countInContinuousRange (lowRange, highRange);
+	  output[numOfContinuosSamples].upperLimit = highRange;
+	  output[numOfContinuosSamples].lowerLimit = lowRange;
+	  output[numOfContinuosSamples].readingsCount = readingsCount;
+	  printOnConsole (output,numOfContinuosSamples);
+	  numOfContinuosSamples++;
+	}
+
     }
-	return currentRangeReadings;
+  return output;
 }
 
-RangeAndReadings* checkRangeAndReadings(int *readings,int length)
-{  
-   return(  findContinuousSamples(readings, length));
-    
+rangeAndReadings* findRangeAndReadings (int *readings, int length)
+{
+    rangeAndReadings* numOfContinuosSamples = 0;
+  numOfContinuosSamples = findContinuousSamples (readings, length);
+  return numOfContinuosSamples;
+  
+   
 }
-
