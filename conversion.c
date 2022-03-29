32d740a3a43191cdb667952c7f69a9979be371a4
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-03-29 09:50:48 +0530

    Extension addition

--- conversion.c
+++ conversion.c
@@ -57,3 +57,8 @@ int *conversionInternalToPhysical (float *rawValues,int arraysize, int A2D_resol
    return absValue;
 }
  
+rangeAndReadings* convertAndCollectRangs(float *rawValues,int arraysize,int A2D_resolution,float PhyMin, float PhyMax,void(*fpPrintOnConsole)(rangeAndReadings*,int) , void(*fpPrintErrorMessage)(void))
+	{
+		int * currentRange = conversionInternalToPhysical (rawValues, arraysize,  A2D_resolution, PhyMin, PhyMax,fpPrintErrorMessage);
+		return (findRangeAndReadings (currentRange ,arraysize,fpValidateArray,fpPrintOnConsole,fpPrintErrorMessage) ; );
+	}
